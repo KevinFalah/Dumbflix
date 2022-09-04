eb3d5fd11667a62ed5d02312c2067aae9effe53d
KevinFalah <kevinghossanfalah34@gmail.com>
2022-09-05 00:34:44 +0700

    update list movie

--- src/components/AddMovie/ListFilm.jsx
+++ src/components/AddMovie/ListFilm.jsx
@@ -12,30 +12,22 @@ function ListFilm() {
   console.log(category)
   return (
     <>
-    <div className='d-flex mt-4 ms-5'>
-      <div className='d-flex flex-row gap-4'>
+    <div className='d-flex mt-4 px-5  justify-content-between'>
+      <div className='d-flex gap-4'>
         <h4 className='text-white'>List Film</h4>
         <select value={category} onChange={(e) => setCategory(() => e.target.value)} className='bg-black text-white' name="list" id="list">
-          <option disabled>Category</option>
           <option>TV Series</option>
           <option>Movie</option>
         </select>
       </div>
-      <div style={{marginLeft: "920px"}}>
-        <Button className="btn bg-danger text-white border-0 px-4" as={Link} to='/add-film'>Add Film</Button>
+      <div>
+        <Button className="btn bg-danger text-white border-0 px-5" as={Link} to='/add-film'>Add Film</Button>
       </div>
     </div>
     
     <div className='sectionSeries'>
       {category == "TV Series" ? 
-      (
-        <SeriesContainer /> 
-      )
-      :
-      (
-        <MovieContainer />
-      )
-    }
+      ( <SeriesContainer /> ):(<MovieContainer />)}
     </div>
       
     </>
